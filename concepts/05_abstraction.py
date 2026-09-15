982587cdbe554df7e350fafbcebb215026d80301
HarithaMihimal <harithamihimal4@gmail.com>
2026-09-15 23:34:52 +0530

    Add Shape and Rectangle classes with area calculation

---
+++ concepts/05_abstraction.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+rectangle = Rectangle(10, 5)
+print(f"Rectangle area: {rectangle.area()}")
